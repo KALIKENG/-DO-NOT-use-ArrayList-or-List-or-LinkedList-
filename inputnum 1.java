d610e5af509994a6ebe2833eaa7ceac0791cbb7f
KALIKENG <156284066+KALIKENG@users.noreply.github.com>
2024-02-20 23:30:41 -1200

    Add files via upload

---
+++ inputnum 1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+        int[] numbers = new int[n];
+        System.ossut.println("Enter " + n + " numbers:");
+        for (int i = 0; i < n; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+        System.out.println("Input numbers:");
+        for (int i = 0; i < n; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
